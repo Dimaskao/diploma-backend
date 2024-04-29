1da91f6be68eeee8cdb05fc68450667a4c433677
Володимир Nagual <nahualli.ya@gmail.com>
2024-04-29 23:24:02 +0300

    create JobOffer model

---
+++ app/Models/JobOffer.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Model;
+
+class JobOffer extends Model
+{
+    use HasFactory, HasUuids;
+
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'title',
+        'company_id',
+        'position',
+        'desc',
+        'requirements',
+        'requirement_experience'
+    ];
+
+    public function company(): BelongsTo
+    {
+        return $this->belongsTo(Company::class);
+    }
+
+    public function skills(): BelongsToMany
+    {
+        return $this->belongsToMany(Skill::class);
+    }
+}
