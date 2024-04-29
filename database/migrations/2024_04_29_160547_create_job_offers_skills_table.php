020214659291de8ca1a6849bc441d163703351a9
Володимир Nagual <nahualli.ya@gmail.com>
2024-04-30 00:02:54 +0300

    create job_offers__skills pivot migration

---
+++ database/migrations/2024_04_29_160547_create_job_offers_skills_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('job_offers_skills', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->uuid('job_offer_id');
+            $table->uuid('skill_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('job_offers_skills');
+    }
+};
