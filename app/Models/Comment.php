3801634796470e72f9ee94f01c1cbddcf72c7147
Володимир Nagual <nahualli.ya@gmail.com>
2024-04-29 23:23:04 +0300

    create Comment model

---
+++ app/Models/Comment.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    use HasFactory, HasUuids;
+
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'text',
+        'post_id',
+        'user_id',
+        'company_id'
+    ];
+
+    public function post(): BelongsTo
+    {
+        return $this->belongsTo(Post::class);
+    }
+}
