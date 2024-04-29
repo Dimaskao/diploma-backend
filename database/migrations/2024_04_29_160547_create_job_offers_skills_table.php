481c0d8190a8863e96e6fe20f88c962b714b19b4
Володимир Nagual <nahualli.ya@gmail.com>
2024-04-30 00:03:57 +0300

    edit job_offers__skills pivot migration

--- database/migrations/2024_04_29_160547_create_job_offers_skills_table.php
+++ database/migrations/2024_04_29_160547_create_job_offers_skills_table.php
@@ -14,7 +14,9 @@ return new class extends Migration
         Schema::create('job_offers_skills', function (Blueprint $table) {
             $table->uuid('id')->primary();
             $table->uuid('job_offer_id');
+            $table->foreign('job_offer_id')->references('id')->on('job_offers')->onDelete('cascade');
             $table->uuid('skill_id');
+            $table->foreign('skill_id')->references('id')->on('skills')->onDelete('cascade');
             $table->timestamps();
         });
     }
