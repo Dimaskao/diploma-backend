8e5bf1f203d07927db78c2410239fe46d5c3d079
Володимир Nagual <nahualli.ya@gmail.com>
2024-04-29 23:25:19 +0300

    add relations to Skill model

--- app/Models/Skill.php
+++ app/Models/Skill.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Model;
 
 class Skill extends Model
@@ -12,4 +13,14 @@ class Skill extends Model
 
     protected $primaryKey = 'id';
     protected $fillable = ['name'];
+
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class);
+    }
+
+    public function job_offers(): BelongsToMany
+    {
+        return $this->belongsToMany(JobOffer::class);
+    }
 }
