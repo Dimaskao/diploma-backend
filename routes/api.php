1f57d95ba1a8e0a459dff317da05dc5952248ab5
Володимир Nagual <nahualli.ya@gmail.com>
2024-06-14 00:04:29 +0300

    add route to get all joboffers by company

--- routes/api.php
+++ routes/api.php
@@ -9,11 +9,14 @@ use Illuminate\Support\Facades\Route;
 Route::resource('users', UsersController::class);
 Route::resource('job-offers', JobOffersController::class);
 
+Route::get('job-offers-by-coid/{id}', [JobOffersController::class, 'job_offers_by_company']);
+
 Route::get('skills', [SkillsController::class, 'index']);
 Route::get('skills/{id}', [SkillsController::class, 'show']);
 
 Route::get('companies', [CompaniesController::class, 'index']);
 Route::get('companies/{id}', [CompaniesController::class, 'show']);
+Route::post('companies', [CompaniesController::class, 'store']);
 
 // Route::get('job-offers', [JobOffersController::class, 'index']);
 // Route::get('job-offers/{id}', [JobOffersController::class, 'show']);
