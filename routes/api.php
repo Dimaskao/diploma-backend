8cfd6bcd513a0bf54ae6c8b2e0880d7c151b726e
xxwonderwallxx <vika010501@gmail.com>
2024-06-14 17:41:25 -0400

    + Update routes

--- routes/api.php
+++ routes/api.php
@@ -1,40 +1,20 @@
 <?php
 
-// use App\Http\Controllers\CompaniesController;
-// use App\Http\Controllers\JobOffersController;
-// use App\Http\Controllers\SkillsController;
-// use App\Http\Controllers\UsersController;
-// use Illuminate\Support\Facades\Route;
-
-// Route::resource('users', UsersController::class);
-
-// Route::get('skills', [SkillsController::class, 'index']);
-// Route::get('skills/{id}', [SkillsController::class, 'show']);
-
-// Route::get('companies', [CompaniesController::class, 'index']);
-// Route::get('companies/{id}', [CompaniesController::class, 'show']);
-
-// Route::get('job-offers', [JobOffersController::class, 'index']);
-// Route::get('job-offers/{id}', [JobOffersController::class, 'show']);
-
-
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-// Register a new user or company
+// Public Routes
 Route::post('/register', [AuthController::class, 'register']);
-
-// Log in user or company
 Route::post('/login', [AuthController::class, 'login']);
 
-// Log out user or company
-Route::post('/logout', [AuthController::class, 'logout']);
+// Protected Routes
+Route::middleware('auth:api')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
 
-// Display the specified user or company profile
-Route::get('/profile/{id}', [AuthController::class, 'show']);
-
-// Update the specified user or company profile
-Route::put('/profile/{id}', [AuthController::class, 'update']);
-
-// Add contact information to user or company profile
-Route::post('/profile/{id}/contact', [AuthController::class, 'addContactInfo']);
+    Route::get('/profile/{id}', [ProfileController::class, 'show']);
+    Route::put('/profile/{id}', [ProfileController::class, 'update']);
+    Route::post('/profile/{id}/subscribe', [ProfileController::class, 'subscribe']);
+    Route::post('/profile/{id}/unsubscribe', [ProfileController::class, 'unsubscribe']);
+    Route::get('/search', [ProfileController::class, 'search']);
+});
