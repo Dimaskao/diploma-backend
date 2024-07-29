9b66580f5859e275016e0e20999650090be43b82
xxwonderwallxx <vika010501@gmail.com>
2024-07-29 00:24:00 -0400

    - Profile refactoring. Bug fixes. Regular user profile successful tests

--- app/Services/Profile/SpecificProfile/RegularUser/Handlers/Update/Helpers/SkillsHelper.php
+++ app/Services/Profile/SpecificProfile/RegularUser/Handlers/Update/Helpers/SkillsHelper.php
@@ -17,30 +17,12 @@ trait SkillsHelper
     private function updateUserSkills($skills, $user): array
     {
         $result = [];
-        $skillsTest = Skill::all()->toArray();
-//        Log::debug('skills from db: ' . var_export($skillsTest, 1));
 
-//        Log::debug('skills init: ' . var_export($skills, 1));
         foreach ($skills as $skill) {
-//            Log::debug('skill: ' . var_export($skill, 1));
-//            Log::debug('skill[id]: ' . var_export($skill['id'], 1));
-//            Log::debug('skill[edit_info]: ' . var_export($skill['edit_info'], 1));
-
-//            if (!isset($skill['id'])) {
-//                Log::error('Skill id was not set');
-//            }
-
 
             if (isset($skill['id']) && isset($skill[Edit::EDIT_INFO])) {
                 $editInfo = $skill[Edit::EDIT_INFO];
                 $skillId = $skill['id'];
-//                if ($editInfo === Edit::ADD) {
-//                    // Add skill logic here
-//                    Log::debug("Adding skill with ID: $skillId");
-//                } elseif ($editInfo === Edit::REMOVE) {
-//                    // Remove skill logic here
-//                    Log::debug("Removing skill with ID: $skillId");
-//                }
 
                 $result[] = match ($editInfo) {
                     Edit::ADD => $this->addSkill($skillId, $user),
